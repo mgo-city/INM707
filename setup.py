9b92b4ab3ef0d4417fd869d3e6086b28414578d7
mgo-city <114659016+mgo-city@users.noreply.github.com>
2023-03-28 21:14:12 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+# read the contents of the README file
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name="lab",
+    version="0.0.1",
+    description="Example repository for INM 702",
+    author="Michael Garcia Ortiz",
+    author_email="michael.garcia-ortiz@city.ac.uk",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=requirements,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
---
+++ src/lab_02/exercise.py
@@ -0,0 +1,2 @@
+def example_function(a, b):
+    return a * b
